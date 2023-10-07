43908fb7e280f9853b07d6addb13a6c379f44176
mohamed amin <mohamedamin0az@gmail.com>
2023-10-07 04:29:22 +0300

    deliver parcels api

--- controllers/bikerController.js
+++ controllers/bikerController.js
@@ -30,10 +30,56 @@ export const pickUp = (req, res) => {
   }
 };
 
-export const bikerParcels = ()=>{
+export const bikerParcels = (req, res) => {
   try {
-    
-  } catch (error) {
-    console.log(error);
+    if (req.type == "Biker") {
+      Parcel.findAll({
+        where: [
+          {
+            bikerId: req.userId,
+          },
+          {
+            status: "pickedUp",
+          },
+        ],
+      })
+        .then((data) => {
+          res.status(200).json(data);
+        })
+        .catch((e) => {
+          res.status(500).json(e);
+        });
+    } else {
+      res.status(500).json("wrong credentials");
+    }
+  } catch (e) {
+    console.log("error", e);
   }
-}
+};
+
+export const deliver = (req, res) => {
+  try {
+    if (req.type == "Biker") {
+      Parcel.update(
+        { status: "delivered"},
+        {
+          where: {
+            id: req.params.id,
+          },
+        }
+      )
+        .then(() => {
+          return res
+            .status(200)
+            .json(`parcel has been delivered  successfully `);
+        })
+        .catch((e) => {
+          return res.status(500).json(`something went wrong" ${e}`);
+        });
+    } else {
+      return res.status(500).json(`wrong credentials`);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
--- routes/bikerRoute.js
+++ routes/bikerRoute.js
@@ -1,7 +1,12 @@
 import express from "express";
-import { pickUp, bikerParcels } from "../controllers/bikerController.js";
+import {
+  pickUp,
+  bikerParcels,
+  deliver,
+} from "../controllers/bikerController.js";
 const router = express.Router();
 router.get("/pickup/parcel/:id", pickUp);
+router.get("/deliver/parcel/:id", deliver);
 router.get("/biker-parcels", bikerParcels);
 
 
